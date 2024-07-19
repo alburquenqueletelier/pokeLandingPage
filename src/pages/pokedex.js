3a360af89c649235faf0c817543334294c5ba151
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-19 17:31:58 -0400

    Se crea vista pokedex con la data del pokemon seleccionado

--- src/pages/pokedex.js
+++ src/pages/pokedex.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 
 const Pokedex = () => {
+    const location = useLocation();
+    const data = location.state?.poke;
+    console.log("location: ", location, "data", data)
+
+    if (!data){
+        return             (<div className="spinner-grow" role="status">
+        <span className="visually-hidden">Loading...</span>
+    </div>)
+    };
 
     return(
-        <>
-        <p>este es pokedex</p>
-        </>
+        <div className="container">
+            <h1>Pokédex Information</h1>
+            <div className="row">
+                <div className="col-auto">
+                    <p>#{data.id} {data.name}</p>
+                    <img src={data.image} alt={data.name} className="card-img-top"/>
+                </div>
+                <div className="col-auto">
+                    <div className="d-block">
+                        <p>Tpyes: {data.type}</p>
+                        <p>Height: {data.height}</p>
+                        <p>Weight: {data.weight}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <p>{data.description}</p>
+            </div>
+        </div>
     )
 };
 
