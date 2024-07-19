75cc4251e66a9feca841b923ccc56d0c983f19c0
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-19 09:50:50 -0400

    Error de sintaxis de importación fixed

--- src/layout.js
+++ src/layout.js
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/home.js';
+import Home from "./pages/home.js";
 import Grid from "./pages/grid.js";
 import Pokedex from "./pages/pokedex.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokegrid" element={<Grid />} />
-        <Route path="/pokedex" element={<Pokedex />} />
-      </Routes>
-    </BrowserRouter>
-    
+
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokegrid" element={<Grid />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
