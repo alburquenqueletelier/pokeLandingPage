3c368bb6d35a0bf7e1eebeb5039fadb8311cfa4f
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-21 15:26:41 -0400

    fixed position

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -4,41 +4,64 @@ import { Context } from "../store/appContext";
 import getImageByUrl from "../utils/utils";
 import PokePagination from "../components/pagination";
 import CardGrid from "../components/cardGrid";
+import SearchForm from "../components/searchForm";
+import FavButton from "../components/favButton";
 
 const Grid = () => {
 
     const { store, actions } = useContext(Context);
     const currentPage = store.page;
+    
+    const handleClick = (e) => {
+        actions.setShowFavs();
+        console.log("show Favs: ", store.showFavs);
+    };
 
     return (
-        <div>
-            {/* <form className="pt-2 ">
-                <div className="d-flex">
-                    <label htmlFor="search" className="form-label">Search by Name</label>
-                    <input type="text" name="search" className="form-control w-50" placeholder="e.g Pikachu" onChange={handleSearch}/>
-                </div>
-            </form> */}
+        <div className="py-2">
+
             <div className="d-flex justify-content-center">
                 <PokePagination />
+                <button onClick={handleClick}>filter by favs</button>
             </div>
-                <div className="row justify-content-center">
-                    {store?.pokeNames && store.page
-                    ? store.pokeNames.slice((currentPage-1)*30, currentPage*30).map((poke, index)=>{
-                        return(
-                            <div className="col-4 col-md-auto" key={index}>
-                                <CardGrid 
-                                    name={poke.name}
-                                    image={getImageByUrl(poke.url)}
-                                    getPokeInfo={actions.getPokeInfo}
-                                />
-                            </div>
-                        )
-                    })
-                    :<p>Cargando...</p>
-                    }
-                    
-                </div>
-            <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center pb-1">
+                <SearchForm />
+            </div>
+            <div className="row justify-content-center">
+                {
+                    !store.showFavs && store?.pokeNames
+                        ? store.pokeNames.slice((currentPage - 1) * 30, currentPage * 30).map((poke, index) => {
+                            return (
+                                <div className="col-4 col-md-auto" key={index}>
+                                    <CardGrid
+                                        name={poke.name}
+                                        image={getImageByUrl(poke.url)}
+                                        getPokeInfo={actions.getPokeInfo}
+                                    />
+                                    <FavButton key={index} poke={poke}/>
+                                </div>
+                            )
+                        })
+                        : store?.showFavs
+                        ?
+                        store.favs.map((poke, index) => {
+                            return (
+                                <div className="col-4 col-md-auto" key={index}>
+                                        <CardGrid
+                                            name={poke.name}
+                                            image={getImageByUrl(poke.url)}
+                                            getPokeInfo={actions.getPokeInfo}
+                                        />
+                                        <FavButton key={index} poke={poke}/>
+                                    </div>
+                                )
+                            })
+                            : <p>Cargando...</p>
+
+                }
+
+            </div>
+            <div className="d-flex justify-content-center py-2">
                 <PokePagination />
             </div>
         </div>
