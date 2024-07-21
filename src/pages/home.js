50e87fbe139358d20a101573a6cf1315a9710b09
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-21 08:34:24 -0400

    El boton dice start

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
     return (
         <div className="d-flex justify-content-center align-items-center">
-            <Link to={"/pokegrid"} className="btn btn-danger">ASDSA</Link>
+            <Link to={"/pokegrid"} className="btn btn-danger">START!</Link>
         </div>
     );
 };
