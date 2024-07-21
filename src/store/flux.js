8bf2357100d8cfe19268a8e4c447f9f698725c75
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-20 20:16:48 -0400

    getPokeInfo entrega descripcion como texto. Se agregan logs antes y desupues de fetch

--- src/store/flux.js
+++ src/store/flux.js
@@ -11,51 +11,46 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
             getPokeNames: () => {
+				console.log("getPokeNames start");
                 fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=2000")
                 .then(response => response.json())
                 .then(data=>{
                     setStore({pokeNames: data.results})
                     localStorage.setItem('pokeNames', JSON.stringify(data.results));
+					console.log("getPokeNames success")
                 })
 				.catch(error => console.log(error))
             },
-			getPokeList: (start=0, end=30) => {
-				fetch(`https://pokeapi.co/api/v2/pokemon?offset=${start}&limit=${end}`)
-				.then(response => response.json())
-                .then(data=>{
-                    setStore({pokeNames: data})
-                    localStorage.setItem('pokeNames', JSON.stringify(data));
-                })
-				.catch(error => console.log(error))
-			},
 			getPokeInfo: (poke) =>{
 				const {pokeInfo} = getStore();
-				if (pokeInfo.filter(data => data.id === poke || data.name === poke)){
+				if (pokeInfo.filter(data => data.id === poke || data.name === poke).length > 0){
 					console.log("Poke already in system");
 				} else {
-
+					console.log("getPokeInfo start");
 					fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`)
 					.then(response => response.json())
-					.then(pokeInfo=>{
-						fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeInfo.id}`)
+					.then(pokeFetched=>{
+						fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeFetched.id}`)
 						.then(response => response.json())
 						.then(speciesInfo => {
 							let info = {
-								id: pokeInfo.id,
-								name: pokeInfo.name,
-								height: pokeInfo.height,
-								weight: pokeInfo.weight,
-								types: pokeInfo.types.map(data => data.type.name),
-								description: speciesInfo.flavor_text_entries.filter(text => text.language.name === "en")[0],
-								image: pokeInfo.sprites.other["official-artwork"].front_shiny
-										? pokeInfo.sprites.other["official-artwork"].front_shiny
-										: pokeInfo.sprites.front_shiny
+								id: pokeFetched.id,
+								name: pokeFetched.name,
+								height: pokeFetched.height,
+								weight: pokeFetched.weight,
+								types: pokeFetched.types.map(data => data.type.name),
+								description: speciesInfo.flavor_text_entries.filter(text => text.language.name === "en")[0].flavor_text,
+								image: pokeFetched.sprites.other["official-artwork"].front_shiny
+										? pokeFetched.sprites.other["official-artwork"].front_shiny
+										: pokeFetched.sprites.front_shiny
 							};
 							pokeInfo.push(info);
 							setStore({pokeInfo: pokeInfo});
+							console.log("getPokeInfo success");
 						})
 						.catch(error => console.log(error))
 					})
+					.catch(error => console.log(error))
 				}
 			},			
 			loadInfo: (type)=>{
