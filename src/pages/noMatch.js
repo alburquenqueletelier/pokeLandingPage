396339a8972141d29ed93306b886b07879e545bf
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-22 15:57:44 -0400

    Se agrego boton volver atrás

--- src/pages/noMatch.js
+++ src/pages/noMatch.js
@@ -1,10 +1,12 @@
 import React from "react";
+import BackButton from "../components/backButton";
 
 const NoMatch = () => {
     return (
         <div className="row">
             <div className="col-4">
-                <h3>We don't have that Route. Please come back or hit the logo to return</h3>
+                <h3>We don't have that Route. Please come back</h3>
+                <BackButton />
             </div>
         </div>
     )
