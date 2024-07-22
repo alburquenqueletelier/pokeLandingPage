7768fcf29f0fda364d8d5b239c3812589491e4cb
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-22 15:56:43 -0400

    Mensaje de error 404 con opción para volver al home

---
+++ src/pages/noMatch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoMatch = () => {
+    return (
+        <div className="row">
+            <div className="col-4">
+                <h3>We don't have that Route. Please come back or hit the logo to return</h3>
+            </div>
+        </div>
+    )
+};
+
+export default NoMatch;
