74efd50520e078d45ebdfa9bbb763ef3cef4c1d6
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-22 15:56:03 -0400

    Ahora muestra mensaje de error en rutas no validas

--- src/layout.js
+++ src/layout.js
@@ -1,10 +1,11 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Link } from "react-router-dom";
 import injectContext from "./store/appContext";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import Home from "./pages/home.js";
 import Grid from "./pages/grid.js";
 import Pokedex from "./pages/pokedex.js";
+import NoMatch from "./pages/noMatch.js"
 
 function Layout() {
   const location = useLocation();
@@ -17,6 +18,7 @@ function Layout() {
               <Route path="/" element={<Home />} />
               <Route path="/pokegrid/:page" element={<Grid />} />
               <Route path="/pokedex/:poke" element={<Pokedex />} />
+              <Route path="*" element={<NoMatch />} />
             </Routes>
         </CSSTransition>
       </TransitionGroup>
