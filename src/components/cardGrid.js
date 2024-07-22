af31b4a5b29b54c83b3cbe163ea9448c50403309
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2024-07-22 15:08:41 -0400

    Se agrega spinner mientras se carga la imagen

--- src/components/cardGrid.js
+++ src/components/cardGrid.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 const CardGrid = ({name, image, getPokeInfo}) => {
 
+    const [loadingImg, setLoadingImg] = useState(true);
+
+    const handleLoad = () => {
+        setLoadingImg(false);
+    };
+
     return(
-        <Link to={`/pokedex/${name}`} onClick={() => getPokeInfo(name)}>
-            <div className="card" style={{width: "18rem"}}>
-                <h5 className="card-header">{name}</h5>
-                <img src={image} className="card-img-top" alt={name}/>
-                <div className="card-body">
+        <div>
+            {
+                loadingImg && <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>
+            }
+            <Link to={`/pokedex/${name}`} onClick={() => getPokeInfo(name)} style={{display: loadingImg ? "none" : "block"}}>
+                <div className="card" style={{width: "18rem"}}>
+                    <h5 className="card-header">{name}</h5>
+                    <img src={image} className="card-img-top" alt={name} onLoad={handleLoad}/>
+                    <div className="card-body">
+                    </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+
+        </div>
     )
 };
 
