134c218740d684fdf4c87210b66edeb9e77d80ed
baal <alburquenque.letelier@gmail.com>
2025-02-09 14:03:14 -0300

    Added lazeload on images

--- src/components/cardGrid.js
+++ src/components/cardGrid.js
@@ -1,30 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Spinner from "react-bootstrap/esm/Spinner";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css'; // Importa los estilos para el efecto blur
+import { CSSTransition } from 'react-transition-group'; // Importa CSSTransition
 
-const CardGrid = ({name, image}) => {
+const CardGrid = ({ name, image }) => {
 
     const [loadingImg, setLoadingImg] = useState(true);
+    const [error, setError] = useState(false);
 
     const handleLoad = () => {
         setLoadingImg(false);
     };
 
-    const handleError = (image) => {
-        setLoadingImg(false);
-        console.log("There is an issue with the image: ", image);
+    const handleError = () => {
+        setLoadingImg(true); // Considera la imagen como cargada (sin éxito)
+        setError(true); // Marca que hubo un error
+        console.error("Error loading image:", image); // Usa console.error para errores
     };
 
-    return(
+
+    return (
         <div>
             {
                 loadingImg && <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>
             }
             <Link to={`/pokedex/${name}`} className={loadingImg ? "d-none text-decoration-none" : "d-block text-decoration-none"}>
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card" style={{ width: "18rem" }}>
                     <h5 className="card-header">{name}</h5>
                     <div className="card-body">
-                    <img src={image} className="card-img-top" alt={name} onLoad={handleLoad} onError={()=>handleError(image)}/>
+                        <LazyLoadImage
+                            src={image}
+                            alt={name}
+                            effect="blur" // Aplica un efecto blur mientras se carga la imagen
+                            onLoad={handleLoad} // Establece imageLoaded a true cuando la imagen se carga
+                            onError={handleError}
+                            useIntersectionObserver={true}
+                            className="pokemon-image" // Clase para la imagen
+                        />
+                        <CSSTransition
+                            in={loadingImg}
+                            timeout={300} // Duración de la transición
+                            classNames="fade-in" // Clases para la transición
+                            unmountOnExit // Elimina el componente del DOM cuando termina la transición
+                        >
+                            <img src={image} className="card-img-top" alt={name} onLoad={handleLoad} onError={() => handleError(image)} />
+                        </CSSTransition>
+                        {error && <p className="text-danger">Error al cargar la imagen.</p>}
                     </div>
                 </div>
             </Link>
--- src/pages/pokedex.js
+++ src/pages/pokedex.js
@@ -4,19 +4,29 @@ import { Context } from "../store/appContext";
 import BackButton from "../components/backButton";
 import Spinner from "react-bootstrap/esm/Spinner";
 import FavButton from "../components/favButton";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { CSSTransition } from 'react-transition-group';
 
 const Pokedex = () => {
 
-    const {store, actions} = useContext(Context);
-    const {poke} = useParams();
+    const { store, actions } = useContext(Context);
+    const { poke } = useParams();
     const [pokeToRender, setPokeToRender] = useState();
     const [loadingImg, setLoadingImg] = useState(true);
+    const [error, setError] = useState(false);
 
     const handleLoad = () => {
         setLoadingImg(false);
     };
 
-    useEffect(()=>{
+    const handleError = () => {
+        setLoadingImg(true); // Considera la imagen como cargada (sin éxito)
+        setError(true); // Marca que hubo un error
+        console.error("Error loading image:", pokeToRender.image); // Usa console.error para errores
+    };
+
+    useEffect(() => {
         const fetchData = async () => {
             await actions.getPokeInfo(poke);
             const pokeToRender = store.pokeInfo.filter(dataPoke => dataPoke.name === poke)[0];
@@ -30,22 +40,39 @@ const Pokedex = () => {
         };
     },)
 
-    return(
-        
+    return (
+
         <div className="container border rounded border-secondary border-2 mt-1 px-1" >
-            
+
             <div className="d-flex">
-            <BackButton/>
-            <h1 className="m-auto text-decoration-underline">Pokédex Information</h1>
+                <BackButton />
+                <h1 className="m-auto text-decoration-underline">Pokédex Information</h1>
 
             </div>
-                {
-                    pokeToRender 
+            {
+                pokeToRender
                     ?
                     <div className="row">
                         <div className="col-7 col-sm-3">
                             <h5 className="text-center border border-1 mt-1">#{pokeToRender.id} {pokeToRender.name}</h5>
-                            <img className="card-img-top bg-light bg-opacity-75 rounded mb-1" src={pokeToRender.image} alt={pokeToRender.name} />
+                            <LazyLoadImage
+                                src={pokeToRender.image}
+                                alt={pokeToRender.name}
+                                effect="blur"
+                                onLoad={handleLoad}
+                                onError={handleError} // Manejo de errores en LazyLoadImage
+                                className="pokemon-image card-img-top" // Clase card-img-top aquí
+                                useIntersectionObserver={true} // Mejora el rendimiento con Intersection Observer
+                            />
+                            <CSSTransition
+                                in={loadingImg}
+                                timeout={300}
+                                classNames="fade-in"
+                                unmountOnExit
+                            >
+                                <img className="card-img-top bg-light bg-opacity-75 rounded mb-1" src={pokeToRender.image} alt={pokeToRender.name} onLoad={handleLoad} onError={handleError} />
+                            </CSSTransition>
+                            {error && <p className="text-danger">Error al cargar la imagen.</p>}
 
                         </div>
                         <div className="col-12 col-sm-9">
@@ -64,13 +91,13 @@ const Pokedex = () => {
                             <FavButton poke={store.pokeNames.filter(data => data.name === poke)[0]} />
                         </div>
                     </div>
-                    : <div className="d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
+                    : <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
                         <Spinner animation="border" role="status"><span className="visually-hidden">Loading...</span></Spinner>
                     </div>
 
-                }        
+            }
         </div>
-        
+
     )
 };
 
